699357c332fa7220f59f915402100168248c55a0
wadiih <wadiih@epactunis.com>
2024-10-14 18:01:15 +0100

    fix non build issue

--- src/main/java/com/example/order_application/entities/Product.java
+++ src/main/java/com/example/order_application/entities/Product.java
@@ -1,27 +1,22 @@
 package com.example.order_application.entities;
 
-import com.example.order_application.entities.Category;
-import com.example.order_application.entities.OrderDetail;
-import jakarta.persistence.CascadeType;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
-
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table
+@Table(name = "products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
